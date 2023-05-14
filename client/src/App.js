62e4eb7d5218124d75a02c8941d02db52c388182
Vaibhav Mani <vaibvinesh@gmail.com>
2023-05-13 22:22:46 -0700

    linking deployed frontend and backend

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,7 @@ import Home from "./components/Home"
 import ChatPage from "./components/ChatPage";
 import socketIO from "socket.io-client"
 
-const socket = socketIO.connect("https://chat-application-vaibvinesh.vercel.app/")
+const socket = socketIO.connect("https://chat-application-self-nine.vercel.app/")
 function App() {
   return (
     <BrowserRouter>
