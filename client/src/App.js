a0afb4cb80c74187597ec8537e038200c9dd9eff
Vaibhav Mani <vaibvinesh@gmail.com>
2023-05-13 21:10:32 -0700

    adding dependencies

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,7 @@ import Home from "./components/Home"
 import ChatPage from "./components/ChatPage";
 import socketIO from "socket.io-client"
 
-const socket = socketIO.connect("http://localhost:4000")
+const socket = socketIO.connect("https://chat-application-vaibvinesh.vercel.app/")
 function App() {
   return (
     <BrowserRouter>
